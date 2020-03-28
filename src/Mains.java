1cb84794b1cc2391b791cb23acbbf754fc954693
turqutagashov <turqutagasov@gmail.com>
2020-03-28 23:05:29 +0400

    Class

--- src/Mains.java
+++ src/Mains.java
@@ -2,12 +2,9 @@ public class Mains {
 
     public static void main(String[] args) {
         House houseOne = new House();
-        houseOne.heightOfHouse = "5 metres";
+        houseOne.heightOfHouse = "6 metres";
         
         houseOne.owner.information("Turqut", "male", 17);
-
-
-
     }
 
 }
